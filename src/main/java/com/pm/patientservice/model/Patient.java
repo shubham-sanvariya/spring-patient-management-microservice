3ec0264ed7c68dc072dc244e9fe9d73b9967b4af
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-07-18 18:49:05 +0530

    first commit

---
+++ src/main/java/com/pm/patientservice/model/Patient.java
@@ -0,0 +1,35 @@
+package com.pm.patientservice.model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Patient {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @NotNull
+  @Email
+  @Column(unique = true)
+  private String email;
+
+  @NotNull
+  private String address;
+
+  @NotNull
+  private LocalDate dateOfBirth;
+
+  @NotNull
+  private LocalDate registeredDate;
+
+}
