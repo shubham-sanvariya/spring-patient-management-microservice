782b64e785f4d8e6aa0c0c3b2d3285d08e2eadd7
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-07-20 01:10:15 +0530

    Add createPatient method to PatientService for patient registration

--- src/main/java/com/pm/patientservice/service/PatientService.java
+++ src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,11 +1,14 @@
 package com.pm.patientservice.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 
 @Service
@@ -25,5 +28,15 @@ public class PatientService {
                 .stream()
                 .map(p -> patientMapper.toProductDto(p))
                 .toList();
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO dto){
+        Patient patient = patientMapper.toPatient(dto);
+
+        patient.setRegisteredDate(LocalDate.now());
+
+        patient = patientRepository.save(patient);
+        
+        return patientMapper.toProductDto(patient);
     }
 }
