d6404a1afe1096ae339b180949c6ca77f024719e
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-07-20 01:10:07 +0530

    Refactor PatientMapper to update method signatures and mapping configurations for improved patient data handling

--- src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -3,14 +3,15 @@ package com.pm.patientservice.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
 
 @Mapper(componentModel = "spring")
 public interface PatientMapper {
     
-    @Mapping(target = "registeredDate", ignore = true)
-    Patient toProductEntity(PatientResponseDTO dto);
+    @Mapping(target = "id", ignore = true)
+    Patient toPatient(PatientRequestDTO entity);
 
     PatientResponseDTO toProductDto(Patient patient);
 }
