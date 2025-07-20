b7cf868fab82642a43110cb64129c396a5620403
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-07-20 16:04:40 +0530

    Refactor PatientService to improve email existence check and update patient creation method

--- src/main/java/com/pm/patientservice/service/PatientService.java
+++ src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,12 +1,15 @@
 package com.pm.patientservice.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.pm.patientservice.dto.PatientCreateDTO;
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
+import com.pm.patientservice.exception.PatientNotFoundException;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -30,14 +33,29 @@ public class PatientService {
                 .toList();
     }
 
-    public PatientResponseDTO createPatient(PatientRequestDTO dto){
-        if (patientRepository.existsByEmail(dto.email())) {
-            throw new EmailAlreadyExistsException("A patient with this email already exists " + dto.email());
+    public void emailExists(String email) {
+        if (patientRepository.existsByEmail(email)) {
+            throw new EmailAlreadyExistsException("A patient with this email already exists " + email);
         }
+    }
+
+    public PatientResponseDTO createPatient(PatientCreateDTO dto){
+        emailExists(dto.email());
         Patient patient = patientMapper.toPatient(dto);
 
         patient = patientRepository.save(patient);
         
         return patientMapper.toProductDto(patient);
     }
+
+    public PatientResponseDTO updatePatient(UUID id, PatientRequestDTO dto){
+        Patient patient = patientRepository.findById(id).orElseThrow(() -> new PatientNotFoundException("Patient not found with ID: " + id));
+
+        emailExists(dto.email());
+
+        patient = patientMapper.toPatient(dto,patient);
+
+        return patientMapper.toProductDto(patientRepository.save(patient));
+    }
+
 }
