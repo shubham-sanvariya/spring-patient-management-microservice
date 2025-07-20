7fcbb31cffa196235d2daa734725d3d52dbee92a
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-07-20 14:00:25 +0530

    Add existsByEmail method to PatientRepository for email existence check

--- src/main/java/com/pm/patientservice/repository/PatientRepository.java
+++ src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.pm.patientservice.model.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, UUID>{
-    
+    boolean existsByEmail(String email);
 }
