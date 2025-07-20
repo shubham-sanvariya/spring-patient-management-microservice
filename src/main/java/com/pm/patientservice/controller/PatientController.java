054cb430d6440da3413bb98e7a1d81501b2f68b5
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-07-20 16:04:45 +0530

    Refactor PatientController to update createPatient method signature and response status

--- src/main/java/com/pm/patientservice/controller/PatientController.java
+++ src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -1,14 +1,19 @@
 package com.pm.patientservice.controller;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pm.patientservice.dto.PatientCreateDTO;
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.service.PatientService;
@@ -31,7 +36,12 @@ public class PatientController {
     }
 
     @PostMapping
-    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO dto){
-        return ResponseEntity.ok().body(patientService.createPatient(dto));
+    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientCreateDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(patientService.createPatient(dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @Valid @RequestBody PatientRequestDTO dto){
+        return ResponseEntity.ok().body(patientService.updatePatient(id, dto));
     }
 }
