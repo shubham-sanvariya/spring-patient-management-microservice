5004474363e8f12a48a6ab0857f828e3e2432b7c
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-07-20 16:04:28 +0530

    Refactor PatientMapper to update method signatures for PatientCreateDTO and PatientRequestDTO

--- src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -2,7 +2,9 @@ package com.pm.patientservice.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
+import com.pm.patientservice.dto.PatientCreateDTO;
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
@@ -11,7 +13,11 @@ import com.pm.patientservice.model.Patient;
 public interface PatientMapper {
     
     @Mapping(target = "id", ignore = true)
-    Patient toPatient(PatientRequestDTO entity);
+    Patient toPatient(PatientCreateDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "registeredDate", ignore = true)
+    Patient toPatient(PatientRequestDTO dto, @MappingTarget Patient patient);
 
     PatientResponseDTO toProductDto(Patient patient);
 }
