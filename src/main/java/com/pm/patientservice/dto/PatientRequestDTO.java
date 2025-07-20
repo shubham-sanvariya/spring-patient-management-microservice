f41b88d2460df42d08eb4dfd355c2502de384bae
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-07-20 15:50:23 +0530

    Refactor PatientRequestDTO to improve field order and formatting

--- src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
+++ src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -5,22 +5,18 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record PatientRequestDTO(
-    @NotBlank(message = "Name is required")
-    @Size(max = 100, message = "Name cannot exceed 100 characters")
-    String name,
-    
-    @NotBlank(message = "email is required")
-    @Email(message = "Email should be valid")
-    String email,
+        @NotBlank(message = "Name is required")
+        @Size(max = 100, message = "Name cannot exceed 100 characters")
+        String name,
 
-    @NotBlank(message = "Address is required")
-    String address,
+        @NotBlank(message = "email is required")
+        @Email(message = "Email should be valid")
+        String email,
 
-    @NotBlank(message = "Date of birth is required")
-    String dateOfBirth,
+        @NotBlank(message = "Address is required")
+        String address,
 
-    @NotBlank(message = "Registered Date is required")
-    String registeredDate
-    ) {
-    
+        @NotBlank(message = "Date of birth is required")
+        String dateOfBirth
+) {
 }
